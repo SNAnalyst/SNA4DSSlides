cd1e031e392300164e70543dfa44b78dbc3c16fd
snanalyst <r.t.a.j.leenders@tilburguniversity.edu>
2026-09-01 15:21:42 +0200

    week 1 lecture 1

---
+++ convert_slides.R
@@ -0,0 +1,2 @@
+tb <- RLslides::xaringan_slide_urls("D:/Dropbox/R/eigen_packages/=git/SNA4DSSlides/Week01_Lecture_Introduction.html", exclude = 33:36)
+RLslides::turn_to_pdf(tb, folder = "D:/Dropbox/R/eigen_packages/=git/SNA4DSSlides/Week01_Lecture_Introduction")
