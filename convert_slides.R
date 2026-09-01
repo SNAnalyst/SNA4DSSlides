9c2ff6d1f94e66f77eec0341519bae770ee785bd
snanalyst <r.t.a.j.leenders@tilburguniversity.edu>
2026-09-01 16:41:58 +0200

    lecture measures

--- convert_slides.R
+++ convert_slides.R
@@ -1,2 +1,10 @@
 tb <- RLslides::xaringan_slide_urls("D:/Dropbox/R/eigen_packages/=git/SNA4DSSlides/Week01_Lecture_Introduction.html", exclude = 33:36)
 RLslides::turn_to_pdf(tb, folder = "D:/Dropbox/R/eigen_packages/=git/SNA4DSSlides/Week01_Lecture_Introduction")
+
+
+
+tb <- RLslides::xaringan_slide_urls("D:/Dropbox/R/eigen_packages/=git/SNA4DSSlides/Lecture_01b_Rcoding_CZ.html")
+RLslides::turn_to_pdf(tb, folder = "D:/Dropbox/R/eigen_packages/=git/SNA4DSSlides/Lecture_01b_Rcoding_CZ")
+
+
+
